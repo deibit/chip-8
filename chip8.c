4821a05ea5c46adfb1641d0fbca56dad79a229e1
David Garcia <david.garcianunez@telefonica.com>
2021-07-29 23:27:52 +0200

    added some conditional debug functions

--- chip8.c
+++ chip8.c
@@ -1,3 +1,13 @@
+/*
+    CHIP-8 emulator (David García - 2021)
+
+    Interesting links:
+      (guide)     https://tobiasvl.github.io/blog/write-a-chip-8-emulator/
+      (test rom)  https://github.com/daniel5151/AC8E/tree/master/roms
+      (reference) https://github.com/daniel5151/AC8E/blob/master/references/Cowgod's%20Chip-8%20Technical%20Reference.pdf
+      (test rom)  https://github.com/corax89/chip8-test-rom
+*/
+
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -18,6 +28,21 @@
 
 #define PROGRAM_START_ADDRESS 0x200
 #define MAX_PROGRAM_MEMORY 0x1000 - 0x200
+
+#define DEBUG 1
+
+#if DEBUG
+#define cdebug(...)               \
+  do                              \
+  {                               \
+    fprintf(stderr, __VA_ARGS__); \
+                                  \
+  } while (0)
+#else
+#define cdebug(fmt, ...)
+#endif
+
+// https://stackoverflow.com/questions/1644868/define-macro-for-debug-printing-in-c
 
 uint8_t display[64][32];
 uint8_t keyboard[16]; // 16 keys
@@ -106,6 +131,7 @@ void chip8_print_memory(size_t until)
 /*
   Zero display array
 */
+
 static void clear_display()
 {
   memset(display, 0, sizeof(uint8_t) * 64 * 32);
@@ -124,8 +150,10 @@ static void ascii2bcd(uint8_t arr[3], uint8_t num)
 /*
   Debug
 */
+
 static void debug()
 {
+#if DEBUG
   const char *template = "-------------------------\nI: %04X\tPC: %X\tInst: %04X\tST: %X\tDT: %X\tSP: %X -> (%04X)\n";
   printf(template, I, PC, get_memory_value_at(PC), ST, DT, SP, stack[SP]);
   const char *regs1 = "V0: %x\tV1: %x\tV2: %x\tV3: %x\t";
@@ -136,13 +164,16 @@ static void debug()
   printf(regs3, V[8], V[9], V[0xa], V[0xb]);
   const char *regs4 = "VC: %x\tVD: %x\tVE: %x\tVF: %x\n\n";
   printf(regs4, V[0xc], V[0xd], V[0xe], V[0xf]);
+#endif
 }
 
 /*
   Debug display
 */
-static void debug_display()
+static void
+debug_display()
 {
+#if DEBUG
   for (uint8_t y = 0; y < 32; y++)
   {
     for (uint8_t x = 0; x < 64; x++)
@@ -151,7 +182,12 @@ static void debug_display()
     }
     printf("\n");
   }
+#endif
 }
+
+/*
+  Debug opcode
+*/
 
 /*
   Initialize CPU and start
@@ -181,6 +217,7 @@ void chip8_step()
     DT--;
   if (ST)
     ST--;
+
   debug();
 
   switch (type)
@@ -190,35 +227,35 @@ void chip8_step()
     {
     case 0x00E0: // Clear display
       clear_display();
-      printf("CLS\n");
+      cdebug("CLS\n");
       PC += 2;
       break;
 
     case 0x00EE: // RET
-      printf("RET\n");
+      cdebug("RET\n");
       PC = stack[SP--];
       break;
 
     default:
-      printf("Opps, unknown 0x0 type opcode: %X\n", opcode);
+      cdebug("Opps, unknown 0x0 type opcode: %X\n", opcode);
       exit(-1);
     }
     break;
 
   case 0x1: // JP nnn
-    printf("JUMP (%X) -> (%X)\n", PC, nnn);
+    cdebug("JUMP (%X) -> (%X)\n", PC, nnn);
     PC = nnn;
     break;
 
   case 0x2: // CALL nnn
     stack[++SP] = PC + 2;
     PC = nnn;
-    printf("CALL %X\n", PC);
+    cdebug("CALL %X\n", PC);
     break;
 
   case 0x3: // SE Vx, byte
     PC += 2;
-    printf("SKIP IF V(%X): (%X), EQUALS byte (%X)\n", x, V[x], kk);
+    cdebug("SKIP IF V(%X): (%X), EQUALS byte (%X)\n", x, V[x], kk);
     if (V[x] == kk)
     {
       PC += 2;
@@ -227,7 +264,7 @@ void chip8_step()
 
   case 0x4: // SNE Vx, byte
     PC += 2;
-    printf("SKIP IF V(%X): (%X) IS NOT %X\n", x, V[x], kk);
+    cdebug("SKIP IF V(%X): (%X) IS NOT %X\n", x, V[x], kk);
     if (V[x] != kk)
     {
       PC += 2;
@@ -238,20 +275,20 @@ void chip8_step()
     PC += 2;
     if (V[x] == V[y])
     {
-      printf("SKIP IF V(%X): %X IS EQUAL V(%X): %X\n", x, V[x], y, V[y]);
+      cdebug("SKIP IF V(%X): %X IS EQUAL V(%X): %X\n", x, V[x], y, V[y]);
       PC += 2;
     }
 
     break;
 
   case 0x6: // LD Vx, byte
-    printf("LOAD V(%x) <- (%X)\n", x, kk);
+    cdebug("LOAD V(%x) <- (%X)\n", x, kk);
     V[x] = kk;
     PC += 2;
     break;
 
   case 0x7: // ADD Vx, byte
-    printf("ADD V(%X) <- %X\n", x, kk);
+    cdebug("ADD V(%X) <- %X\n", x, kk);
     V[x] += kk;
     PC += 2;
     break;
@@ -260,32 +297,32 @@ void chip8_step()
     switch (n)
     {
     case 0: // LD Vx, Vy
-      printf("LOAD V(%X) = V(%X): %X\n", x, y, V[y]);
+      cdebug("LOAD V(%X) = V(%X): %X\n", x, y, V[y]);
       V[x] = V[y];
       PC += 2;
       break;
 
     case 1: // OR Vx, Vy
-      printf("OR V(%X) |= V(%X)\n", x, y);
+      cdebug("OR V(%X) |= V(%X)\n", x, y);
       V[x] = V[x] | V[y];
       PC += 2;
       break;
 
     case 2: // AND Vx, Vy
-      printf("AND V(%X) &= V(%X)\n", x, y);
+      cdebug("AND V(%X) &= V(%X)\n", x, y);
       V[x] = V[x] & V[y];
       PC += 2;
       break;
 
     case 3: // XOR Vx, Vy
-      printf("XOR V(%X) ^= V(%X)\n", x, y);
+      cdebug("XOR V(%X) ^= V(%X)\n", x, y);
       V[x] = V[x] ^ V[y];
       PC += 2;
       break;
 
     case 4: // ADD Vx, Vy
     {
-      printf("ADD Vx(%X) + Vy(%X) (Vf = 1 IF Vx > Vy)\n", x, y);
+      cdebug("ADD Vx(%X) + Vy(%X) (Vf = 1 IF Vx > Vy)\n", x, y);
 
       V[0xF] = V[x] > (UINT8_MAX - V[y]) ? 1 : 0;
       V[x] = V[x] + V[y];
@@ -296,7 +333,7 @@ void chip8_step()
 
     case 5: // SUB Vx, Vy
     {
-      printf("SUB V(%X) - V(%X) (Vf = 1 IF V(x) > V(y))\n", x, y);
+      cdebug("SUB V(%X) - V(%X) (Vf = 1 IF V(x) > V(y))\n", x, y);
 
       V[0xF] = (V[x] > V[y]) ? 1 : 0;
       V[x] = V[x] - V[y];
@@ -307,7 +344,7 @@ void chip8_step()
 
     case 6: // SHR Vx {, Vy}
     {
-      printf("SHR (/2) Vx(%X). IF LSB == 1; Vf = 1 ELSE Vf = 0\n", x);
+      cdebug("SHR (/2) Vx(%X). IF LSB == 1; Vf = 1 ELSE Vf = 0\n", x);
 
       V[0xF] = V[x] & 0x1;
       V[x] = (V[x] >> 1);
@@ -318,7 +355,7 @@ void chip8_step()
 
     case 7: // SUBN Vx, Vy
     {
-      printf("SUBN Vx(%X) = Vy(%X) - Vx(%X) (IF Vx > Vy -> Vf = 1 ELSE 0)\n", x, y, x);
+      cdebug("SUBN Vx(%X) = Vy(%X) - Vx(%X) (IF Vx > Vy -> Vf = 1 ELSE 0)\n", x, y, x);
 
       V[0xF] = (V[y] > V[x]) ? 1 : 0;
       V[x] = V[y] - V[x];
@@ -329,7 +366,7 @@ void chip8_step()
 
     case 0xE: //SHL Vx {, Vy}
     {
-      printf("SHL (*2) Vx(%X) *= 2. IF MSB == 1; Vf = 1 ELSE Vf = 0\n", x);
+      cdebug("SHL (*2) Vx(%X) *= 2. IF MSB == 1; Vf = 1 ELSE Vf = 0\n", x);
 
       V[0xF] = (V[x] >> 7) & 0x1;
       V[x] = (V[x] << 1);
@@ -338,13 +375,13 @@ void chip8_step()
     }
     break;
     default:
-      printf("Opps, unknown 0x8 type opcode: %X\n", opcode);
+      cdebug("Opps, unknown 0x8 type opcode: %X\n", opcode);
       exit(-1);
     }
     break;
 
   case 0x9: // SNE Vx, Vy
-    printf("SKIP IF NOT EQUAL. Vx(%X) != Vy(%X)\n", x, y);
+    cdebug("SKIP IF NOT EQUAL. Vx(%X) != Vy(%X)\n", x, y);
     if (V[x] != V[y])
     {
       PC += 2;
@@ -353,18 +390,18 @@ void chip8_step()
     break;
 
   case 0xA: // LD I, addr
-    printf("LOAD (I) -> %X\n", nnn);
+    cdebug("LOAD (I) -> %X\n", nnn);
     I = nnn;
     PC += 2;
     break;
 
   case 0xB: // JP V0, addr
-    printf("JUMP (%X) + (%X) = (%X)\n", PC, nnn, V[0]);
+    cdebug("JUMP (%X) + (%X) = (%X)\n", PC, nnn, V[0]);
     PC = nnn + V[0];
     break;
 
   case 0xC: // RND Vx, byte
-    printf("RND V(%X) -> rnd & %X\n", x, kk);
+    cdebug("RND V(%X) -> rnd & %X\n", x, kk);
     V[x] = (rand() % 0x256) & kk;
     PC += 2;
     break;
@@ -376,7 +413,7 @@ void chip8_step()
     const uint8_t BITS[8] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};
     V[0xF] = 0;
 
-    printf("DRAW at X: %u Y: %u FROM: %X, %X bytes\n", V[x], V[y], I, n);
+    cdebug("DRAW at X: %u Y: %u FROM: %X, %X bytes\n", V[x], V[y], I, n);
 
     uint8_t col = V[x];
     uint8_t row = V[y];
@@ -410,7 +447,7 @@ void chip8_step()
     case 0xE: // SKP Vx
       if (keyboard[x])
       {
-        printf("KEY DOWN: %X\n", keyboard[x]);
+        cdebug("KEY DOWN: %X\n", keyboard[x]);
         PC += 2;
       }
       PC += 2;
@@ -418,13 +455,13 @@ void chip8_step()
     case 0x1: // SKNP Vx
       if (!keyboard[x])
       {
-        printf("KEY UP: %X\n", keyboard[x]);
+        cdebug("KEY UP: %X\n", keyboard[x]);
         PC += 2;
       }
       PC += 2;
       break;
     default:
-      printf("Opps, unknown 0xE type opcode: %X\n", opcode);
+      cdebug("Opps, unknown 0xE type opcode: %X\n", opcode);
       exit(-1);
     }
     break;
@@ -434,7 +471,7 @@ void chip8_step()
     {
     case 0x07: // LD Vx, DT
       V[x] = DT;
-      printf("SET Vx(%X) <- DELAY %X\n", x, DT);
+      cdebug("SET Vx(%X) <- DELAY %X\n", x, DT);
       PC += 2;
       break;
 
@@ -444,7 +481,7 @@ void chip8_step()
       {
         if (keyboard[i] == 1)
         {
-          printf("WAIT PRESSED...OK\n");
+          cdebug("WAIT PRESSED...OK\n");
           PC += 2;
         }
         break;
@@ -452,26 +489,26 @@ void chip8_step()
       break;
 
     case 0x15: // LD DT, Vx
-      printf("SET DELAY %X -> Vx(%X): %X\n", DT, x, V[x]);
+      cdebug("SET DELAY %X -> Vx(%X): %X\n", DT, x, V[x]);
       DT = V[x];
       PC += 2;
       break;
 
     case 0x18: // LD ST, Vx
-      printf("SET SOUND %X -> Vx(%X): %X\n", ST, x, V[x]);
+      cdebug("SET SOUND %X -> Vx(%X): %X\n", ST, x, V[x]);
       ST = V[x];
       PC += 2;
       break;
 
     case 0x1E: // ADD I, Vx
-      printf("ADD I -> Vx(%X): %X\n", x, V[x]);
+      cdebug("ADD I -> Vx(%X): %X\n", x, V[x]);
       V[0xF] = (I + V[x] > 0xfff) ? 1 : 0;
       I += V[x];
       PC += 2;
       break;
 
     case 0x29: // LD F, Vx
-      printf("LOAD FONT (%X)\n", V[x]);
+      cdebug("LOAD FONT (%X)\n", V[x]);
       I = V[x] * 5;
       PC += 2;
       break;
@@ -484,13 +521,13 @@ void chip8_step()
       memory[I] = arr[0];
       memory[I + 1] = arr[1];
       memory[I + 2] = arr[2];
-      printf("BCD OF %X IS -> I: (%X), I+1: (%X), I+2: (%X)\n", value, arr[0], arr[1], arr[2]);
+      cdebug("BCD OF %X IS -> I: (%X), I+1: (%X), I+2: (%X)\n", value, arr[0], arr[1], arr[2]);
       PC += 2;
     }
     break;
 
     case 0x55: // LD [I], Vx
-      printf("STORE REGISTERS\n");
+      cdebug("STORE REGISTERS\n");
       for (uint8_t i = 0; i <= x; i++)
       {
         memory[I + i] = V[i];
@@ -499,7 +536,7 @@ void chip8_step()
       break;
 
     case 0x65: // LD Vx, [I]
-      printf("RESTORE REGISTERS\n");
+      cdebug("RESTORE REGISTERS\n");
       for (uint8_t i = 0; i <= x; i++)
       {
         V[i] = memory[I + i];
@@ -507,7 +544,7 @@ void chip8_step()
       PC += 2;
       break;
     default:
-      printf("Opps, unknown 0xF type opcode: %X\n", opcode);
+      cdebug("Opps, unknown 0xF type opcode: %X\n", opcode);
       exit(-1);
     }
     //PC += 2;
